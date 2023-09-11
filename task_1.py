12bd69d88268c5e708e8476814b0b8ec0737d609
Mike-P63 <rumaish@rambler.ru>
2023-09-11 19:55:40 +0400

    added task

--- task_1.py
+++ task_1.py
@@ -18,10 +18,24 @@
 
 
 year = int(input('Enter year: '))
-if year % 4 != 0 or year % 100 == 0 and year % 400 !=0:
-    print('Usual year')
+if year % 4 != 0:
+    print('Обычный год')
+elif year % 100 == 0:
+    if year % 400 == 0:
+        print('VISOKOSNIY')
+    else:
+        print('USUAL')
 else:
     print('VISOKOSNIY')
+
+
+    
+
+# year = int(input('Enter year: '))
+# if year % 4 != 0 or year % 100 == 0 and year % 400 !=0:
+#     print('Usual year')
+# else:
+#     print('VISOKOSNIY')
 
 # data = 0
 # while data < 100:
