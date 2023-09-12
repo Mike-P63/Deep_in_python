515c75059e2aad4fe7f1b11ec3020861576d8d07
Mike-P63 <rumaish@rambler.ru>
2023-09-12 10:26:21 +0400

    Canges to task_2 Sem

--- task_2sem.py
+++ task_2sem.py
@@ -8,18 +8,39 @@
 # В коде должны быть один input и один print
 
 
+# number = int(input('Введите число от 1 до 999: '))
+
+# if 0 < number < 10:
+#     print(number ** 2)
+# elif 10 <= number < 100:
+#     number_1 = number % 10
+#     number_2 = number // 10
+#     print(number_1 * number_2)
+# elif 100 <= number < 1000:
+#     number_1 = number % 10
+#     number_2 = (number // 10) % 10
+#     number_3 = number // 100
+#     print(number_1 * 100 + number_2 * 10 + number_3)
+# else:
+#     print('Вы ввели неверное число. Введите число из диапазона 1 - 999')
+
+
 number = int(input('Введите число от 1 до 999: '))
 
-if 0 < number < 10:
-    print(number ** 2)
-elif 10 <= number < 100:
+MIN_NUMBER = 0
+MAX_NUMBER = 1000
+
+if MIN_NUMBER < number < 10:
+    result = number ** 2
+elif 10 <= number < MAX_NUMBER:
     number_1 = number % 10
     number_2 = number // 10
-    print(number_1 * number_2)
-elif 100 <= number < 1000:
+    result = number_1 * number_2
+elif 100 <= number < MAX_NUMBER:
     number_1 = number % 10
     number_2 = (number // 10) % 10
     number_3 = number // 100
-    print(number_1 * 100 + number_2 * 10 + number_3)
+    result = number_1 * 100 + number_2 * 10 + number_3
 else:
     print('Вы ввели неверное число. Введите число из диапазона 1 - 999')
+print(result)
