7f9d9dfd1586cafc2f34de5e036ecd10daaed20f
Mike-P63 <rumaish@rambler.ru>
2023-09-16 12:37:42 +0400

    Added task 2 hw 2

---
+++ home_work/hw_2_task_2.py
@@ -0,0 +1,33 @@
+# Напишите программу, которая получает целое число и
+# возвращает его шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+
+decimal_number = int(input("Введите положительное десятичное число: "))
+print(f'Функция hex = {hex(decimal_number)}')
+hex_dec_rules = "0123456789ABCDEF"
+hex_decimal_number = " "
+
+while decimal_number > 0:
+    n = decimal_number % 16
+    hex_dec_rule = hex_dec_rules[n]
+    hex_decimal_number = hex_dec_rule + hex_decimal_number
+    decimal_number //= 16
+print(f'Результат вычисления = Ox{hex_decimal_number}')
+
+
+# Второй вариант:
+
+
+decimal_number = int(input("Введите положительное десятичное число: "))
+print(f'Функция hex = {hex(decimal_number)}')
+
+def to_hex (number):
+    hex_dec_rules = "0123456789ABCDEF"
+    hex_decimal_number = " "
+    while number > 0:
+        hex_decimal_number = hex_dec_rules[number % 16] + hex_decimal_number
+        number //= 16
+    return '0x' + hex_decimal_number
+
+print(f'Собственная функция: {to_hex(decimal_number)}')
