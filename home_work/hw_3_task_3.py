5d8dd95f360dc1fe5db58ebef29f02a1d5ddadb3
Mike-P63 <rumaish@rambler.ru>
2023-09-19 18:46:11 +0400

    Changes in task 3 hw 3

--- home_work/hw_3_task_3.py
+++ home_work/hw_3_task_3.py
@@ -4,15 +4,15 @@
 # вернуть один допустимый вариант.
 
 
-def rucksack_max_wheith(ITEMS, MAX_WHEITH):
+def rucksack_max_wheith(x, n):
     possible_items = []
-    for item, weight in ITEMS.items():
-        if weight <= MAX_WHEITH:
+    for item, weight in x.items():
+        if weight <= n:
             possible_items.append(item)
-            MAX_WHEITH -= weight
+            n -= weight
     return possible_items
 
 
-ITEMS = {'Топор': 1, 'Вода': 3, 'Консервы': 4, 'Одежда': 3, 'Палатка': 2, 'Котелок': 1 }
+ITEMS = {'Топор': 1, 'Вода': 3, 'Консервы': 2, 'Одежда': 3, 'Палатка': 2, 'Котелок': 1 }
 MAX_WHEITH = 10
 print(f'В рюкзак поместятся вещи: {rucksack_max_wheith(ITEMS, MAX_WHEITH)}')
